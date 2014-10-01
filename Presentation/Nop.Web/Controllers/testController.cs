144e2467ffe6e771c572fbf0a4e3676da6a5919c
赵建伟 <zhaojianwei187@126.com>
2014-10-01 14:58:55 +0800

    Testuploading

---
+++ Presentation/Nop.Web/Controllers/testController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nop.Web.Controllers
+{
+    public class testController : Controller
+    {
+        //
+        // GET: /test/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
